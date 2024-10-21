c6bf9e3c91f4297bd4360a5fb3e798c0e7e5face
lgun <zjavbxj15913@naver.com>
2024-10-21 19:09:50 +0900

    폼 수정, 로그

--- src/main/java/com/example/gunialarm/controller/user/MainController.java
+++ src/main/java/com/example/gunialarm/controller/user/MainController.java
@@ -12,6 +12,7 @@ public class MainController {
     
     @GetMapping("/alarm")
     public String main(HttpServletRequest request, ModelMap model){
+        System.out.println("alarm 페이지 접근");
         return "user/alarm";
     }
 }
